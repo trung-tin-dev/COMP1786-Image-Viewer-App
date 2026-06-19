db6a92fd70e5850d1e771dea098b2f672b914c88
trung-tin-dev <tatin3469@gmail.com>
2026-06-19 13:23:41 +0700

    add image viewer app

---
+++ app/src/main/java/com/example/imageviewerapp/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.imageviewerapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgPhoto;
+    private TextView imgInfo;
+    private Button btnPrevious;
+    private Button btnNext;
+
+    private int currentIndex = 0;
+
+    private final int[] images = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imgPhoto = findViewById(R.id.imgPhoto);
+        imgInfo = findViewById(R.id.imgInfo);
+        btnPrevious = findViewById(R.id.btnPrevious);
+        btnNext = findViewById(R.id.btnNext);
+
+        updateImage();
+
+        btnNext.setOnClickListener(v -> {
+            currentIndex++;
+
+            if (currentIndex >= images.length) {
+                currentIndex = 0;
+            }
+
+            updateImage();
+        });
+
+        btnPrevious.setOnClickListener(v -> {
+            currentIndex--;
+
+            if (currentIndex < 0) {
+                currentIndex = images.length - 1;
+            }
+
+            updateImage();
+        });
+    }
+
+    private void updateImage() {
+        imgPhoto.setImageResource(images[currentIndex]);
+        imgInfo.setText("Image " + (currentIndex + 1) + " of " + images.length);
+    }
+}
